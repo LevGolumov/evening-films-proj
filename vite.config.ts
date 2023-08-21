1393a04c2596808fcd21053474b4f401acf7088e
CyberTechLion <lev.golumov@gmail.com>
2023-08-21 21:24:04 +0400

    Мелкие обновления конфигов

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // checker({
+    //   overlay: { initialIsOpen: false },
+    //   typescript: true,
+    //   eslint: {
+    //     lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+    //   },
+    // }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+  ],
+  server: {
+    port: 3030,
+    // proxy: {
+    //   '/api-server/': '...',
+    //   '/authorization/': '...',
+    // },
+  },
+});
